b4226d53bbf701edf856b34ef8393ada172082cb
Andrew <drace.davidson@gmail.com>
2024-03-02 18:49:22 -0700

    Array problem added

--- week01/code/ArraysTester.cs
+++ week01/code/ArraysTester.cs
@@ -35,11 +35,16 @@ public static class ArraysTester {
     private static double[] MultiplesOf(double number, int length)
     {
         // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
-
-        return new double[0]; // replace this return statement with your own
+        // The easiest way to find the multiples of a given number would be to use the modulus operator as follows:
+        // while: index <= length; if: value of number% == 0, result = number; then index ++;
+        // Or in other words, while the index is less then length variable if the value of number modulus i add it to the list
+        // I think a for loop could do most of it.
+        List<double> multiplesList = new List<double>();
+        for (int i = 0; i < length; i++)
+        {
+            multiplesList.Add(number * (i + 1));
+        }
+        return multiplesList.ToArray();
     }
     
     /// <summary>
@@ -53,9 +58,11 @@ public static class ArraysTester {
     private static void RotateListRight(List<int> data, int amount)
     {
         // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // ok for this one we need to take list data and "shift it" by amount to the right
+        // for example: for example for each item up to range pop item then insert at the end of the list.
+        data.InsertRange(0, data.GetRange(data.Count - amount, amount));
+        data.RemoveRange(data.Count - amount, amount);
+        
 
     }
 }
